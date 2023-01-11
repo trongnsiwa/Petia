d968eacf90933d67e064b8c3324f85f5f8b9db27
Si Trong <trongnsiwa79@gmail.com>
2023-01-11 15:55:57 +0700

    add daisyui and custom theme

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,22 @@
+<script lang ts>
+	import AuthButton from '$lib/components/AuthButton.svelte';
+</script>
+
+<div>
+	<div>
+		<h3>Petia</h3>
+		<h4>Log in</h4>
+		<div>
+			<AuthButton />
+			<AuthButton />
+			<AuthButton />
+		</div>
+		<div>
+			<p>Don't you have an account?</p>
+			<a href="/signup">Sign up</a>
+		</div>
+	</div>
+</div>
+
+<style>
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.svelte', './src/**/*.css'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {},
+		colors: {
+			petia: {
+				100: '#EAE7B1',
+				200: '#A6BB8D',
+				300: '#61876E',
+				400: '#3C6255'
+			},
+			accent: '#383838',
+			neutral: '#B7B7B7',
+			userstory: '#729CAF',
+			bug: '#E85F5F'
+		},
+		fontFamily: {
+			poppins: ['Poppins', 'sans-serif']
+		}
 	},
-	plugins: []
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+	daisyui: {
+		themes: []
+	}
 };
